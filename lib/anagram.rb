d69c10055fe1b64d1f334c29c55535d176dd6cac
Ben <henriquez.ben@gmail.com>
2016-09-29 18:45:32 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+
+class Anagram
+  attr_accessor :word
+  def initialize(word)
+    @word = word
+  end
+
+
+     def match (dictionary_array)
+     dictionary_array.select do |array|
+       array.chars.sort == word.chars.sort
+     end
+   end
+
+
+#split word into array
+#get size of the word
+#iterate through dictionary for each letter of word array
+#get
+end
